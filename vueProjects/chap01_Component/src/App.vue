8e3ce76fef31122783eaab46a518f5479beb9402
gujerbit <ghftm1124@gmail.com>
2021-04-01 18:42:06 +0900

    vue-cli 3.0 (2021-04-01)

---
+++ vueProjects/chap01_Component/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <h1>Vue Components</h1>
+    <temp></temp>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import temp from './components/A01.Currency.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    temp
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
