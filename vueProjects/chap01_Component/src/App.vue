84d5edbf891b8d86de1ff6f7306d34982e3d7ce5
gujerbit <ghftm1124@gmail.com>
2021-04-02 15:40:55 +0900

    vue-cli 3 컴포넌트 연결 (2021-04-02)

--- vueProjects/chap01_Component/src/App.vue
+++ vueProjects/chap01_Component/src/App.vue
@@ -1,32 +1,40 @@
 <template>
-  <div>
-    <img alt="Vue logo" src="./assets/logo.png">
+  <div class="card-body">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <!-- <h1>Vue Components</h1> -->
+    <!-- <temp></temp> -->
     <h1>Vue Components</h1>
-    <temp></temp>
+    <binding></binding>
+    <!-- <binding/> -->
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
-import temp from './components/A01.Currency.vue'
+// import temp from './components/A01.Currency.vue'
+import binding from './components/02_Binding.vue'
 
 export default {
   name: 'App',
   components: {
     // HelloWorld,
-    temp
+    // temp,
+    binding
   }
 }
 </script>
 
 <style>
-#app {
+h1 {
+  color: orange;
+}
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
